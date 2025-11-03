5cc4c40753507ab66fa157cc2a366a8b06bba9d7
hello-avery <jery5237hent@gmail.com>
2025-11-02 21:25:44 -0600

    [init] setup schema

---
+++ models/task.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Task name is required.'] // Validation
+    },
+    deadline: {
+        type: Date,
+        required: [true, 'Task deadline is required.'] // Validation
+    },
+    completed: {
+        type: Boolean,
+        default: false // default
+    },
+    assignedUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null // default
+    },
+    assignedUserName: {
+        type: String,
+        default: 'unassigned' // Reasonable default
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Create and export the model
+module.exports = mongoose.model('Task', TaskSchema);
--- models/user.js
+++ models/user.js
@@ -3,7 +3,25 @@ var mongoose = require('mongoose');
 
 // Define our user schema
 var UserSchema = new mongoose.Schema({
-    name: String
+    name: {
+        type: String,
+        required: [true, 'User name is required.'] // Validation
+    },
+    email: {
+        type: String,
+        required: [true, 'User email is required.'], // Validation
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    pendingTasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task'
+    }],
+    dateCreated: {
+        type: Date,
+        default: Date.now // default
+    }
 });
 
 // Export the Mongoose model
